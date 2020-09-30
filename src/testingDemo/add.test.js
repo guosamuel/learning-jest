47634f9ee5f86af4d51122c4c068b40705bd36e0
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-09-29 22:52:53 -0400

    learned mock functions with jest

--- src/testingDemo/add.test.js
+++ src/testingDemo/add.test.js
@@ -1,12 +1,16 @@
-import { add, total } from './add';
+import { total } from './add';
+
+const add = jest.fn((x, y) => 3);
 
 // Unit test
 // It only tests one thing
 test('add', () => {
   expect(add(1,2)).toBe(3);
-  expect(add(2,5)).toBe(7);
+  expect(add).toHaveBeenCalledTimes(1);
+  expect(add).toHaveBeenCalledWith(1,2);
+  // expect(add(2,5)).toBe(7);
 })
 
-test('total', () => {
-  expect(total(5, 20)).toBe('$25')
-})
+// test('total', () => {
+//   expect(total(5, 20)).toBe('$25')
+// })
