7d41af59e542f0cd18117d63251fd4f99208e68d
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-09-29 22:40:55 -0400

    demo of integration test

--- src/testingDemo/add.test.js
+++ src/testingDemo/add.test.js
@@ -1,10 +1,12 @@
-import { add } from './add';
+import { add, total } from './add';
 
 // Unit test
 // It only tests one thing
 test('add', () => {
-  // const value = add(1,2);
-  // expect(value).toBe(3);
   expect(add(1,2)).toBe(3);
   expect(add(2,5)).toBe(7);
+})
+
+test('total', () => {
+  expect(total(5, 20)).toBe('$25')
 })
